6f05a4c7eaea7ed69c6b360eb0acc425f78e68e5
Awasaky <awasaky@gmail.com>
2017-08-17 22:36:57 +0500

    Start to change layout of all screens to 1280x720

--- game.lua
+++ game.lua
@@ -12,6 +12,22 @@ local scene = composer.newScene()
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
 -- -----------------------------------------------------------------------------------
 
+-- variables to build scene
+local gameLoopTimer
+
+local backGroup -- background group
+local mainGroup -- primary group
+local uiGroup -- information group
+
+local panelFont = native.newFont( "Arial Black" )
+local panelPosY = 70
+
+local textTapsTot
+local textTapsSpd
+local textBelieve
+local textLoadSpd
+local textLoadTot
+
 local json = require( "json" )
 
 -- Сonfiguration files operations
@@ -59,50 +75,6 @@ local function loadConfig()
     end
 end
 
--- Asset loading
--- Asset configuration definition
-local pathAsset = system.pathForFile( "lgcconfig.json", system.DocumentsDirectory )
-
--- объект конфигурации
-local tableAsset = {}
-
--- сохранение конфигурации
-local function saveAsset()
- 
-    local fileConfig = io.open( pathConfig, "w" )
- 
-    if fileConfig then
-        fileConfig:write( json.encode( tableConfig ) )
-        io.close( fileConfig )
-    end
-end
-
--- сброс конфигурации и её сохранение в файл
-local function resetAsset()
-    tableConfig = {
-       	tapsPlayer = 0,
-       	tapsTotal = 0,
-       	loadTotal = 0,
-       	lastTimeSave = os.time()
-   	}
- 	saveConfig()  	
-end
-
-local function loadAsset()
- 
-    local fileConfig = io.open( pathConfig, "r" )
- 
-    if fileConfig then
-        local contents = fileConfig:read( "*a" )
-        io.close( fileConfig )
-        tableConfig = json.decode( contents )
-    end
- 
-    if ( tableConfig == nil or #tableConfig == 0 ) then
-    	resetConfig()
-    end
-end
-
 -- -----------------------------------------------------------------------------------
 -- Scene event functions
 -- -----------------------------------------------------------------------------------
@@ -112,7 +84,55 @@ function scene:create( event )
 
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
-	loadConfig()
+	loadConfig() -- загрузка конфигурации
+
+	-- Set up display groups
+	backGroup = display.newGroup()  -- Display group for the background image
+	sceneGroup:insert( backGroup )  -- Insert into the scene's view group
+ 
+	mainGroup = display.newGroup()  -- Display group for the ship, asteroids, lasers, etc.
+	sceneGroup:insert( mainGroup )  -- Insert into the scene's view group
+ 
+	uiGroup = display.newGroup()    -- Display group for UI objects like the score
+	sceneGroup:insert( uiGroup )    -- Insert into the scene's view group
+
+	-- Load the background
+    -- 720 / 4 == 180
+    local background = display.newImageRect( backGroup, "assets/001/back.png", 1280, 720 )
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+
+    local topbar = display.newImageRect( backGroup, "assets/001/topbar.png", 1600, 90 )
+    topbar.x = display.contentCenterX + 160
+    topbar.y = 45
+
+    textTapsSpd = display.newText( uiGroup, tableConfig.tapsTotal, 160, panelPosY, panelFont, 36 )
+    textTapsSpd:setFillColor( 0, 0, 0 )
+
+    textBelieve = display.newText( uiGroup, tableConfig.tapsTotal, 480, panelPosY, panelFont, 36 )
+    textBelieve:setFillColor( 0, 0, 0 )
+
+    textLoadSpd = display.newText( uiGroup, tableConfig.tapsTotal, 820, panelPosY, panelFont, 36 )
+    textLoadSpd:setFillColor( 0, 0, 0 )
+
+    textLoadTot = display.newText( uiGroup, tableConfig.tapsTotal, 1150, panelPosY, panelFont, 36 )
+    textLoadTot:setFillColor( 0, 0, 0 )
+
+    local shopKey = display.newImageRect( mainGroup, "assets/001/shop.png", 120, 60 )
+    shopKey.x = 60
+    shopKey.y = 130
+
+    local statsKey = display.newImageRect( mainGroup, "assets/001/stats.png", 120, 60 )
+    statsKey.x = 60
+    statsKey.y = 190
+
+    local cheatsKey = display.newImageRect( mainGroup, "assets/001/cheats.png", 120, 60 )
+    cheatsKey.x = 60
+    cheatsKey.y = 250
+
+    local tapKey = display.newImageRect( mainGroup, "assets/001/tapkey.png", 360, 360 )
+    tapKey.x = display.contentCenterX
+    tapKey.y = display.contentCenterY
 end
 
 
--- menu.lua
+++ menu.lua
@@ -26,24 +26,25 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 	
-	local background = display.newImageRect( sceneGroup, "menu/background.png", 480, 320 )
+	local background = display.newImageRect( sceneGroup, "menu/background.png", 1280, 720 )
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
 
-	local title = display.newImageRect( sceneGroup, "menu/title.png", 326, 47 )
-	title.x = display.contentCenterX - 70
-	title.y = display.contentCenterY + 100
+	local title = display.newImageRect( sceneGroup, "menu/title.png", 652, 94 )
+	title.x = display.contentCenterX - 250
+	title.y = display.contentCenterY + 300
 
-	local buttonPlay = display.newImageRect( sceneGroup, "menu/buttonPlay.png", 174, 51 )
-	buttonPlay.x = 390
-	buttonPlay.y = 230
+	local buttonPlay = display.newImageRect( sceneGroup, "menu/buttonPlay.png", 348, 102 )
+	buttonPlay.x = 1050
+	buttonPlay.y = 100
 	
-	local buttonAbout = display.newImageRect( sceneGroup, "menu/buttonAbout.png", 174, 51 )
-	buttonAbout.x = 390
-	buttonAbout.y = 290
+	local buttonAbout = display.newImageRect( sceneGroup, "menu/buttonAbout.png", 348, 102 )
+	buttonAbout.x = 1050
+	buttonAbout.y = 220
 	
 	buttonPlay:addEventListener( "tap", gotoGame )
 	buttonAbout:addEventListener( "tap", gotoAbout )
+	composer.gotoScene( "game" ) -- delete after final objects layout
 end
 
 
